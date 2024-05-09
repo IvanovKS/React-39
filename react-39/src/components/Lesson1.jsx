60496899add2d96e23df1f2be2855eb2904e1a5b
IvanovKS <Skitchi@mail.ru>
2024-05-09 21:51:09 +0300

    feat: add state

--- react-39/src/components/Lesson1.jsx
+++ react-39/src/components/Lesson1.jsx
@@ -2,18 +2,28 @@ import React from 'react';
 import './Lesson1.css';
 
 class Lesson1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 0};
+  }
+
+  incrementNumber = () => {
+    this.setState({ number: this.state.number + 1 });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <h1>Lesson1</h1>
-        <form name="search-form" action="#">
+        <h1>Lesson_1</h1>
+        <form name="number-form" action="#">
           <input
             type="text"
-            placeholder="Search something here..."/>
-          <button type="submit" >Search</button>
+            placeholder="Write any number here..."/>
+          <button type="submit" onClick={this.incrementNumber}>Add + 1</button>
         </form>
+        <p>final number: {this.state.number}</p>
       </React.Fragment>
-  );
+    );
   }
 }
 
