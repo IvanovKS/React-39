09cbf1ea936da8753bc820d311834408095e96db
IvanovKS <Skitchi@mail.ru>
2024-05-11 23:04:18 +0300

    feat: add lifecycle methods

--- react-39/src/components/Lesson1.jsx
+++ react-39/src/components/Lesson1.jsx
@@ -4,14 +4,55 @@ import './Lesson1.css';
 class Lesson1 extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { number: 0};
+    this.state = { 
+      number: 0,
+      hasError: false,
+    };
+    console.log('constructor');
   }
 
   incrementNumber = () => {
     this.setState({ number: this.state.number + 1 });
   }
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('getDerivedStateFromProps');
+    return null;
+  }
+
+  static getDerivedStateFromError(error) {
+    console.log('getDerivedStateFromError');
+    return { hasError: true };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate');
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  componentDidCatch(error, info) {
+    console.log('componentDidCatch');
+  }
+
   render() {
+    console.log('render');
     return (
       <React.Fragment>
         <h1>Lesson_1</h1>
