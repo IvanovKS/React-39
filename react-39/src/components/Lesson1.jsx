15a154db1a352ca6620098cf6d3c08083828da44
IvanovKS <Skitchi@mail.ru>
2024-05-12 22:01:02 +0300

    feat: add disabled button

--- react-39/src/components/Lesson1.jsx
+++ react-39/src/components/Lesson1.jsx
@@ -8,12 +8,17 @@ class Lesson1 extends React.Component {
     this.state = { 
       number: 0,
       hasError: false,
+      inputValue: '',
     };
     console.log('constructor');
   }
 
   incrementNumber = () => {
     this.setState({ number: this.state.number + 1 });
+  }
+
+  handleChange = (event) => {
+    this.setState({ inputValue: event.target.value });
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
@@ -54,14 +59,23 @@ class Lesson1 extends React.Component {
 
   render() {
     console.log('render');
+    const { inputValue } = this.state;
+
     return (
       <React.Fragment>
         <h1>Lesson_1</h1>
         <form name="number-form" action="#">
           <input
+            value={inputValue}
+            onChange={this.handleChange}
             type="text"
-            placeholder="Write any number here..."/>
-          <button type="submit" onClick={this.incrementNumber}>Add + 1</button>
+            placeholder="Write 'реакт' here..."/>
+          {inputValue === "реакт" && (
+            <button disabled={true} type="submit" className="disabled">Disabled</button>
+          )}
+          {inputValue !== "реакт" && (
+            <button disabled={false} type="submit" onClick={this.incrementNumber}>Add + 1</button>
+          )}
         </form>
         <p>final number: {this.state.number}</p>
         <Lesson1Child string="abc" number={10} boolean/>
